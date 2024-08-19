4f00a85b9e7292acd853818e0b822f472a22bc21
osama <osrab3@gmail.com>
2024-08-19 20:54:53 +0300

    add js file

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
     if (loginForm) {
@@ -5,16 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
+
             try {
-                const response = await fetch('http://localhost:3000/login', {
+                const response = await fetch('/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password })
                 });
+
                 const result = await response.json();
+                
                 if (response.ok) {
-                    localStorage.setItem('token', result.token);
-                    alert('Login successful');
+                    localStorage.setItem('token', result.token); 
+                    window.location.href = '/profile';
                 } else {
                     alert(result.message);
                 }
@@ -31,15 +35,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const name = document.getElementById('name').value;
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
+
             try {
-                const response = await fetch('http://localhost:3000/signup', {
+                const response = await fetch('/signup', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ name, email, password })
                 });
+
                 const result = await response.json();
+                
                 if (response.ok) {
-                    alert('Signup successful');
+              
+                    window.location.href = '/profile';
                 } else {
                     alert(result.message);
                 }
